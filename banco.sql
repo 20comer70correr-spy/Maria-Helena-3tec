77c08583c57a44ea1d62f5c95f471a0c013576bc
20comer70correr-spy <anthony.sao@escola.pr.gov.br>
2025-03-28 11:47:49 -0300

    Add files via upload

---
+++ banco.sql
@@ -0,0 +1,25 @@
+CREATE TABLE LOGIN (id int primary key
+name varchar(100),
+email varchar(100000),
+senha varchar(12)
+endereço varchar(200)
+)
+
+CREATE TABLE RECEITA ( id int primary key
+name RECEITA varchar(200)
+modo de preparo TEXT varchar(650)
+)
+
+CREATE TABLE VIDEOS ( id int primary key
+data postagem double int
+duração int
+descrição varchar(100)
+
+)
+
+CREATE TABLE USUARIO ( id int primary key
+name varchar (200)
+endereço varchar (250)
+idade double int
+
+)
